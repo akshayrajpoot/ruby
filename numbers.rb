0b42bbe6c630f3d857382ec72da057152361ce9a
Akshay Singh <89373241+akshayrajpoot@users.noreply.github.com>
2021-09-06 23:03:15 +0530

    Add files via upload

---
+++ numbers.rb
@@ -0,0 +1,16 @@
+puts 2*8
+puts 2**6
+
+puts 10%3
+
+
+
+puts 1+2*6
+num =89
+num+=100
+puts num
+
+num =-67
+puts num.abs()
+puts num.round()
+
---
+++ usrinp.rb
@@ -0,0 +1,12 @@
+puts "Enter your name: "
+name = gets
+puts "Hello #{name}, how are you" #.chomp is used to remove the line
+
+puts "Enter first num: "
+num1 =gets.chomp
+puts "Enter second num: "
+num2 = gets.chomp
+
+puts "The result is " 
+puts  num1.to_f + num2.to_f 
+
